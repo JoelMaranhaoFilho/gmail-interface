6f6af2f36025cb2998463a090bfb968707f2001d
Joel <joel.bmpatriota@gmail.com>
2024-10-13 20:40:45 -0300

    Projeto gmail

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'], // Define a raiz como a pasta src
+          alias: {
+            '@': './src', // O alias @ aponta para src
+          },
+        },
+      ],
+      'react-native-reanimated/plugin', // Adicionando o plugin de animação
+    ],
   };
 };
